2cc2b9e318d0dc9105e21f9c512bc3efea82dbd9
Gábor Fekete <gabor.balazs.fekete@gmail.com>
2017-02-27 09:09:19 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import kivy
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import RenderContext
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.factory import Factory
+from kivy.storage.jsonstore import JsonStore
+from kivy.properties import StringProperty, ObjectProperty, NumericProperty
+from kivy.base import EventLoop
+from kivy.lang import Builder
+from kivy.animation import Animation
+from kivy.uix.textinput import TextInput
+
+class RangeInput(TextInput):
+    min_value = NumericProperty(0)
+    max_value = NumericProperty(100)
+        
+    
+    def insert_text(self, substring, from_undo=False):
+        s = self.text+str(substring)
+        try:
+            if self.min_value <= int(s) <= self.max_value:
+                pass
+        except:
+            s = self.text
+        return super().insert_text(s, from_undo=from_undo)
+
+class ShaderView(FloatLayout):
+    fs = StringProperty()
+    disabled_value = NumericProperty(0)
+    time = 0
+    dtime = 1 / 60.
+    mouse_pos = [0,0]
+
+    def __init__(self,**kwargs):
+        self.canvas = RenderContext()
+        super(ShaderView,self).__init__(**kwargs)
+        Clock.schedule_interval(self.update_shader,self.dtime)
+
+    def update_shader(self, *args):
+        if self.disabled_value == 0:
+            s = self.canvas
+            s['time'] = self.time
+            s['resolution'] = list(map(float, self.size))
+            s['mouse'] = list(map(float, self.mouse_pos))
+            s['projection_mat'] = Window.render_context['projection_mat']
+            s.ask_update()
+            self.time = self.time + self.dtime
+
+    def on_fs(self, instance, value):
+        shader = self.canvas.shader
+        old_value = shader.fs
+        try:
+            shader.fs = value
+        except:
+            raise
+        if not shader.success:
+            shader.fs = old_value
+        else:
+            print('Compiled!')
+    
+    def reset_time(self):
+        self.time = 0
+
+def on_touch_move(touch):
+    mouse_pos = touch.pos
+    print(touch)
+
+class GameApp(App):
+    store = JsonStore('fs_shader.json')
+    fs = StringProperty()
+
+    def on_start(self):
+        if self.store.exists('shader'):
+            self.fs = self.store.get('shader')['fragment']
+        pass
+
+    def on_stop(self):
+        self.store.put('shader',fragment=self.fs)
+        pass
+
+    def build(self):
+        self.ids = self.root.ids
+        self.root.on_touch_move = on_touch_move
+        
+    def toggle_fullscreen(self, toggle_value):
+        self.root_window.borderless = toggle_value
+        self.root_window.resizable = toggle_value
+        self.root_window.fullscreen = [False,'auto'][toggle_value]
+
+if __name__ == '__main__':
+    GameApp(title='Shader').run()
---
+++ shader.py
@@ -0,0 +1,37 @@
+import kivy
+from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.storage.jsonstore import JsonStore
+
+fs_header = '''
+#ifdef GL_ES
+    precision highp float;
+#endif
+uniform vec2 resolution;
+uniform float time;
+'''
+
+class ShaderEditor(BoxLayout):
+    pass
+
+class ShaderApp(App):
+    store = JsonStore('fs_shader.json')
+    shader_target = ObjectProperty()
+
+    def __init__(self,*args,**kwargs):
+        if 'shader_target' in kwargs:
+            self.shader_target = kwargs['shader_target']
+            del kwargs['shader_target']
+        super(ShaderApp,self).__init__(*args,**kwargs)
+
+    def on_start(self):
+        if self.store.exists('shader'):
+            self.root.fs = self.store.get('shader')['fragment']
+
+    def on_stop(self):
+        self.store.put('shader',fragment=self.root.fs)
+
+    def build(self):
+        self.root = ShaderEditor()
+        self.root.fs = fs_header
